4a479edd6e97c4203e9091c055a9b00c7fac9fd2
mmarang <161336039+mmarang@users.noreply.github.com>
2024-06-21 10:57:23 +0200

    Add files via upload

---
+++ final assignment/results/dike_model_simulation_policy_space.py
@@ -0,0 +1,70 @@
+import ema_workbench
+from ema_workbench import Model, MultiprocessingEvaluator, Policy, Scenario
+
+from ema_workbench.em_framework.evaluators import perform_experiments
+from ema_workbench.em_framework.samplers import sample_uncertainties
+from ema_workbench.util import ema_logging
+import time
+
+from fontTools.varLib.instancer import solver
+
+from problem_formulation import get_model_for_problem_formulation
+
+from ema_workbench.em_framework.salib_samplers import get_SALib_problem
+from SALib.analyze import sobol
+from ema_workbench import Samplers
+from ema_workbench import MultiprocessingEvaluator
+
+
+if __name__ == "__main__":
+    ema_logging.log_to_stderr(ema_logging.INFO)
+
+    dike_model, planning_steps = get_model_for_problem_formulation(3)
+
+    # Build a user-defined scenario and policy:
+    reference_values = {
+        "Bmax": 175,
+        "Brate": 1.5,
+        "pfail": 0.5,
+        "ID flood wave shape": 4,
+        "planning steps": 2,
+    }
+    reference_values.update({f"discount rate {n}": 3.5 for n in planning_steps})
+    scen1 = {}
+
+    for key in dike_model.uncertainties:
+        name_split = key.name.split("_")
+
+        if len(name_split) == 1:
+            scen1.update({key.name: reference_values[key.name]})
+
+        else:
+            scen1.update({key.name: reference_values[name_split[1]]})
+
+    ref_scenario = Scenario("reference", **scen1)
+
+    # no dike increase, no warning, none of the rfr
+    zero_policy = {"DaysToThreat": 0}
+    zero_policy.update({f"DikeIncrease {n}": 0 for n in planning_steps})
+    zero_policy.update({f"RfR {n}": 0 for n in planning_steps})
+    pol0 = {}
+
+    for key in dike_model.levers:
+        s1, s2 = key.name.split("_")
+        pol0.update({key.name: zero_policy[s2]})
+
+    policy0 = Policy("Policy 0", **pol0)
+
+
+    problem = get_SALib_problem(dike_model.uncertainties)
+    print(problem) #Get this from the slurm-######.out file
+
+    # Uncertainty space
+    # Sampler = Sobol on lever sampling
+    # n_scenarios = reference_scenario
+    n_policies = 256
+
+    with MultiprocessingEvaluator(dike_model) as evaluator:
+        results = evaluator.perform_experiments(scenarios=ref_scenario, policies=n_policies, lever_sampling=Samplers.SOBOL)
+
+    ema_workbench.util.utilities.save_results(results, 'policy_space_refsc_256pol.tar.gz')
